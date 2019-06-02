dc4085ee298cc23d552e63f5165e8e993ba1c20e
Esteban Lara <esteban@esnilara.io>
2019-06-02 13:38:44 -0500

    feat: Update application adapter to include access_token in request headers

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,14 @@
 import DS from 'ember-data';
+import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
 
 export default DS.JSONAPIAdapter.extend({
-  namespace: 'api'
+  namespace: 'api',
+  authManager: service(),
+
+  headers: computed('authManager.accessToken', function() {
+    return {
+      'Authorization': `Bearer ${this.get('authManager.accessToken')}`
+    };
+  })
 });
