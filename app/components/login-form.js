615e2b9ef0af1856b2dc3f023a79180533b3ae0e
Esteban Lara <esteban@esnilara.io>
2019-06-02 12:28:17 -0500

    feat: Add authenticate action in login-form component that uses auth-manager service

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -1,8 +1,19 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
+  authManager: service(),
+
   actions: {
-    authenticate() {
+    async authenticate() {
+      try {
+        const { username, password } = this.getProperties('username', 'password');
+        await this.get('authManager').authenticate(username, password);
+
+        alert('Success! Click the top link!');
+      } catch (err) {
+        alert(`Error obtaining token: ${err.responseText}`);
+      }
     }
   }
 });
