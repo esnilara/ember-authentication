24e9ec24bd1882a1f5ea42facbc5a9a7e21eba1f
Esteban Lara <esteban@esnilara.io>
2019-06-04 21:58:25 -0500

    feat: Use ember-simple-auth session in login-form and pass authenticator

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -2,13 +2,13 @@ import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 
 export default Component.extend({
-  authManager: service(),
+  authManager: service('session'),
 
   actions: {
     async authenticate() {
       try {
         const { username, password } = this.getProperties('username', 'password');
-        await this.get('authManager').authenticate(username, password);
+        await this.get('authManager').authenticate('authenticator:oauth2', username, password);
 
         alert('Success! Click the top link!');
       } catch (err) {
