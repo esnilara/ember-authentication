792ea5e53a80157b03cfe6e7c10f744d9d1df6fa
Esteban Lara <esteban@esnilara.io>
2019-06-04 22:59:40 -0500

    feat: Change secret-box protection with login route transition

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  actions: {
+    error() {
+      this.transitionTo('/login');
+      return false;
+    }
+  }
 });
--- app/routes/secrets.js
+++ app/routes/secrets.js
@@ -1,11 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  async model() {
-    try {
-     return await this.store.findAll('code');
-    } catch (e) {
-      return [];
-    }
+  model() {
+    return this.store.findAll('code');
   }
 });
